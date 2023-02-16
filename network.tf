7dae1f304b243484e147002bb369c0234bb5630a
root <root@ip-172-31-93-18.ec2.internal>
2023-02-16 00:41:36 +0000

    Add: Create IGW

--- network.tf
+++ network.tf
@@ -47,7 +47,14 @@ resource "aws_subnet" "vorx-subnet-priv-1b" {
     Name = "prod-private-subnet-1b"
   }
 }
- 
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vorx-vpc-prod.id
+
+  tags = {
+    Name = "igw-prod-vorx-vpc"
+  }
+} 
 
 ## OUTPUTS DO NOSSO TF ##
 output "vpc_vorx_prod_id" {
